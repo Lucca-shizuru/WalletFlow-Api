ee494ad5a2f2a56f79f7387b07c699d7f4930dce
Lucca Shizuru <luccashizuru@gmail.com>
2026-02-26 15:27:08 -0300

    Iniciando projeto/ Feature/ Criação da primeira entidade

---
+++ WalletFlow.Domain/Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletFlow.Domain.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; private set; }
+        public string Owner { get; private set; }
+        public decimal Balance { get; private set; }
+
+        public Account(string owner)
+        {
+            Id = Guid.NewGuid();
+            Owner = owner;
+            Balance = 0;
+
+            if (string.IsNullOrWhiteSpace(owner))
+            {
+                throw new ArgumentException("Owner name cannot be empty.");
+            }
+        }
+
+
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawal amount must be positive.");
+            }
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+            Balance -= amount;
+        }
+    }
+}
+     
+
