98ed07411c214a75a8759a266989d9d6ab9709ac
Lucca Shizuru <luccashizuru@gmail.com>
2026-03-24 00:32:30 -0300

    Feature/ Criação do repository

--- WalletFlow.Infrastructure/Context/WalletFlowContext.cs
+++ WalletFlow.Infrastructure/Context/WalletFlowContext.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
 using WalletFlow.Domain.Entities;
 
 namespace WalletFlow.Infrastructure.Context
 {
     public class WalletFlowContext : DbContext
     {
-    }
+        public WalletFlowContext(DbContextOptions<WalletFlowContext> options) : base(options) { }
+
+        
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasKey(a => a.Id);
+            modelBuilder.Entity<Account>().Property(a => a.Balance).HasPrecision(18, 2);
+
+            modelBuilder.Entity<Transaction>().HasKey(t => t.Id);
+            modelBuilder.Entity<Transaction>().Property(t => t.Amount).HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
 }
---
+++ WalletFlow.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WalletFlow.Domain.Entities;
+using WalletFlow.Domain.Repositories.Interfaces;
+
+namespace WalletFlow.Domain.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+       
+        public Task AddAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
