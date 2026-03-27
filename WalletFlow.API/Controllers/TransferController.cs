75c719499e58f7736f9a3a8615571961e51f3094
Lucca Shizuru <luccashizuru@gmail.com>
2026-03-26 22:31:51 -0300

    Feature/ Criação da transferController

---
+++ WalletFlow.API/Controllers/TransferController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalletFlow.API.Dtos;
+using WalletFlow.Application.Interfaces;
+using WalletFlow.Application.Services;
+
+namespace WalletFlow.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+
+        public TransferController(ITransferService traferService)
+        {
+            _transferService = traferService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer([FromBody] TransferRequest request)
+        {
+            var result = await _transferService.ExecuteTransfer(request.FromId, request.ToId, request.Amount);
+
+            if (!result.IsSuccess)
+                return BadRequest(new { error = result.Error });
+
+            return Ok(new { message = "Transferência concluída!" });
+        }
+
+
+    }
+}
---
+++ WalletFlow.API/Dtos/TransferRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WalletFlow.API.Dtos
+{
+    public record TransferRequest(Guid FromId, Guid ToId, decimal Amount);
+    
+    
+}
