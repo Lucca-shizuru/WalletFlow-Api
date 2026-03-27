ec1808745493b3c79543d706892e5210c66fae8a
Lucca Shizuru <luccashizuru@gmail.com>
2026-03-26 21:49:18 -0300

    Feature/ configurando appsetings e program.cs

--- WalletFlow.API/Program.cs
+++ WalletFlow.API/Program.cs
@@ -1,41 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using WalletFlow.Infrastructure.Context;
+using WalletFlow.Application.Interfaces;
+using WalletFlow.Application.Services;
+using WalletFlow.Domain.Repositories;
+using WalletFlow.Domain.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// 1. Configurar o Entity Framework com PostgreSQL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<WalletFlowContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// 2. Registrar os Repositórios (Infra -> Domain)
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+
+// 3. Registrar os Serviços de Aplicação (Application)
+builder.Services.AddScoped<ITransferService, TransferService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
